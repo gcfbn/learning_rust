e26f1fb4be0ba0f2176d389f44ec976669e9a70c
gcfbn <bmekarski@interia.pl>
2021-06-15 10:50:18 +0200

    Fix Kruskal's algorithm (missing find_parent)

--- kruskals_algorithm/src/kruskal/algorithm.rs
+++ kruskals_algorithm/src/kruskal/algorithm.rs
@@ -18,12 +18,18 @@ pub fn calculate_min_total_weight(graph: structures::Graph) -> i32 {
     // sort result_edges by ascending weight
     result_edges.sort_by(|a, b| a.weight.cmp(&b.weight));
 
+    let mut from_parent: u32 = 0;
+    let mut to_parent: u32 = 0;
+
     // remove edges that connect nodes with the same parental node
+    // when lambda expression returns false, edge will be removed
     result_edges.retain(|&e| {
-        if find_parent(&parents, e.to_index) == find_parent(&parents, e.from_index) {
+        from_parent = find_parent(&parents, e.from_index);
+        to_parent = find_parent(&parents, e.to_index);
+        if from_parent == to_parent {
             false
         } else {
-            parents[e.to_index as usize] = e.from_index;
+            parents[to_parent as usize] = from_parent;
             true
         }
     });
