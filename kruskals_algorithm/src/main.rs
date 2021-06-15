5a467f5ab3a4fa1d2b87ee4513079c240889ea2c
gcfbn <bmekarski@interia.pl>
2021-06-15 11:43:45 +0200

    Add Clap (command line service)

--- kruskals_algorithm/src/main.rs
+++ kruskals_algorithm/src/main.rs
@@ -1,9 +1,28 @@
+#[macro_use]
+extern crate clap;
+
 use std::process;
 
 fn main() {
-    let result = kruskals_algorithm::run("input.txt").unwrap_or_else(|error| {
-        eprintln!("An error happened: {}", error);
-        process::exit(1);
-    });
-    println!("{}", result);
+    // generate menu using Clap
+    let matches = clap_app!(kruskal_algorithm =>
+        (version: "1.0")
+        (author: "Bartek M. <bmekarski@interia.pl>")
+        (about: "Algorithms & Data structures task from graph theory")
+        (@arg TASK: -t --task "Prints info about task")
+        (@arg RUN: -r --run +takes_value "Runs the program")
+        )
+        .get_matches();
+
+    if let Some(r) = matches.value_of("RUN") {
+        let result = kruskals_algorithm::run(r).unwrap_or_else(|error| {
+            eprintln!("An error happened: {}", error);
+            process::exit(1);
+        });
+        println!("{}", result);
+    }
+
+    if matches.occurrences_of("TASK") > 0 {
+        println!("Task is available in task.txt");
+    }
 }
