e6b59087f6d916e93b01d7cc68564d2bab5b7bcd
gcfbn <bmekarski@interia.pl>
2021-06-15 19:18:20 +0200

    Add clap commandline menu

--- weather_app/src/main.rs
+++ weather_app/src/main.rs
@@ -1,3 +1,15 @@
+use clap::clap_app;
+
 fn main() {
+    let matches = clap_app!(weather_app =>
+        (version: "1.0")
+        (author: "Bartek M. <bmekarski@interia.pl>")
+        (about: "Weather app using free API")
+        (@arg POLISH: -p --polish "Use polish instead of default english language")
+        (@arg IMPERIAL: -i --imperial "Use imperial units instead of default metrics units")
+        (@arg CITY: -c --city +required +takes_value "Search for weather in city")
+    )
+        .get_matches();
+
     println!("Hello, world!");
 }
