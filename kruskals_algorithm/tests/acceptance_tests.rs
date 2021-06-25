6c248f8031dbb4151d87164ec2521549b64a398b
Paweł Dowgird <pawel.dowgird@anixe.pl>
2021-06-25 20:54:09 +0200

    Some renames: LibResult->Result, KruskalsAlgorithmError->BuildGraphError

--- kruskals_algorithm/tests/acceptance_tests.rs
+++ kruskals_algorithm/tests/acceptance_tests.rs
@@ -1,4 +1,4 @@
-use kruskals_algorithm::{run, KruskalsAlgorithmError};
+use kruskals_algorithm::{run, BuildGraphError};
 use test_case::test_case;
 
 #[test_case(1 => 280)]
@@ -16,6 +16,6 @@ fn passing(dataset_number: u32) -> i32 {
 #[test]
 fn failing_graph_not_connected() {
     let actual = run("tests/data/failing_graph_not_connected.txt").unwrap_err();
-    let expected = KruskalsAlgorithmError::GraphNotConnected;
+    let expected = BuildGraphError::GraphNotConnected;
     assert_eq!(actual.to_string(), expected.to_string());
 }
