33d96c5fb61b2affad4a978c11fc72cd95f8de44
gcfbn <bmekarski@interia.pl>
2021-06-28 13:53:08 +0200

    Issue #11 (part 2 - last) Add acceptance test cases

--- kruskals_algorithm/tests/acceptance_tests.rs
+++ kruskals_algorithm/tests/acceptance_tests.rs
@@ -41,6 +41,14 @@ fn passing(dataset_number: u32) -> i32 {
         }
 })]
 #[test_case("error_not_enough_data", BuildGraphError::NotEnoughData)]
+#[test_case("error_parsing_graph_parameters_n", BuildGraphError::ParsingError{
+    parameter_name: String::from("n"),
+    value: String::from("X"),
+})]
+#[test_case("error_parsing_graph_parameters_m", BuildGraphError::ParsingError{
+parameter_name: String::from("m"),
+value: String::from("X"),
+})]
 fn test_graph_building_errors(graph_file: &str, expected_error: BuildGraphError) {
     let actual_error = run(format!(
         "tests/data/error_tests/graph_building_errors/{}.txt",
@@ -50,12 +58,22 @@ fn test_graph_building_errors(graph_file: &str, expected_error: BuildGraphError)
     assert_eq!(actual_error.to_string(), expected_error.to_string());
 }
 
-#[test_case("error_edge_description_bad_from_index", CreatingEdgeError::from_edge_description_with_bad_from_index(&EdgeDescription {
+#[test_case("error_edge_description_bad_from_index", BuildGraphError::from(CreatingEdgeError::from_edge_description_with_bad_from_index(&EdgeDescription {
 from_index: "xyz",
 to_index: "3",
 weight: "100",
-}))]
-fn test_creating_edge_errors(graph_file: &str, expected_error: CreatingEdgeError) {
+})))]
+#[test_case("error_edge_description_bad_to_index", BuildGraphError::from(CreatingEdgeError::from_edge_description_with_bad_to_index(&EdgeDescription {
+from_index: "1",
+to_index: "abc",
+weight: "150",
+})))]
+#[test_case("error_edge_description_bad_weight", BuildGraphError::from(CreatingEdgeError::from_edge_description_with_bad_weight(&EdgeDescription {
+from_index: "1",
+to_index: "2",
+weight: "10a0",
+})))]
+fn test_creating_edge_errors(graph_file: &str, expected_error: BuildGraphError) {
     let actual_error = run(format!(
         "tests/data/error_tests/creating_edge_errors/{}.txt",
         graph_file
