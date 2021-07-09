9cbaa954e12c6feab2e82456683ff3ad995fe2e0
gcfbn <bmekarski@interia.pl>
2021-07-09 12:27:10 +0200

    Add example to lib.rs file

--- graphs/graph/src/lib.rs
+++ graphs/graph/src/lib.rs
@@ -1,6 +1,28 @@
-//! Implementation of graph structure and few graph algorithms
+//! Implementation of directed graph structure
 //!
 //! Result type for functions in this crate is [`crate::errors::Result`]
+//!
+//! # Example
+//! ```
+//! use graph::build_graph_from_string;
+//!
+//! let input = String::from("4 3
+//! 1 2 150
+//! 1 3 220
+//! 4 2 140
+//! ");
+//!
+//! let graph = build_graph_from_string(input).unwrap();
+//!
+//! assert_eq!(graph.nodes_count, 4);
+//! assert_eq!(graph.edges.len(), 3);
+//!
+//! let second_edge = graph.edges[1];
+//!
+//! assert_eq!(second_edge.from_index, 1);
+//! assert_eq!(second_edge.to_index, 3);
+//! assert_eq!(second_edge.weight, 220);
+//! ```
 
 // extern these crates only when running tests
 #[cfg(test)]
