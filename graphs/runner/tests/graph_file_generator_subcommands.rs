97b7730620095338b5f809427d7b66061b85cf94
gcfbn <bmekarski@interia.pl>
2021-07-15 21:26:13 +0200

    Remove redundant module

--- graphs/runner/tests/graph_file_generator_subcommands.rs
+++ graphs/runner/tests/graph_file_generator_subcommands.rs
@@ -1,60 +1,56 @@
 use runner_lib::*;
 
-mod subcommand_graph_file_generator {
+use super::*;
+use anyhow::Result;
+
+mod failing_tests {
     use super::*;
-    use anyhow::Result;
+    use test_case::test_case;
 
-    mod failing_tests {
-        use super::*;
-        use test_case::test_case;
+    #[test]
+    fn fails_because_edges_count_is_to_small() -> Result<()> {
+        let parameters =
+            GraphFileGenerator::try_from_args("--graph-file aaa.txt --nodes-count 5 --edges-count 3 --max-weight 100")
+                .unwrap();
 
-        #[test]
-        fn fails_because_edges_count_is_to_small() -> Result<()> {
-            let parameters = GraphFileGenerator::try_from_args(
-                "--graph-file aaa.txt --nodes-count 5 --edges-count 3 --max-weight 100",
-            )
-            .unwrap();
+        let actual_error = generate_graph(&parameters).unwrap_err();
+        let expected_error = GraphFileGeneratorError::TooFewEdgesForConnectedGraph {
+            edges_count: 3,
+            nodes_count: 5,
+        };
 
-            let actual_error = generate_graph(&parameters).unwrap_err();
-            let expected_error = GraphFileGeneratorError::TooFewEdgesForConnectedGraph {
-                edges_count: 3,
-                nodes_count: 5,
-            };
-
-            assert_eq!(actual_error.to_string(), expected_error.to_string());
-            Ok(())
-        }
-
-        #[test_case("--graph-file aaa.txt --nodes-count 5a --edges-count 3 --max-weight 100"; "nodes count is not integer")]
-        #[test_case("--graph-file aaa.txt --nodes-count 5 --edges-count 3a --max-weight 100"; "edges count is not integer")]
-        #[test_case("--graph-file aaa.txt --nodes-count 5 --edges-count 3 --max-weight 100a"; "max weight is not integer")]
-        #[test_case("--graph-file aaa.txt --nodes-count 5 --edges-count 3a"; "missing max weight")]
-        #[test_case("--graph-file aaa.txt --nodes-count 0 --edges-count 3 --max-weight 100"; "nodes count is zero")]
-        #[test_case("--graph-file aaa.txt --nodes-count -5 --edges-count 3 --max-weight 100"; "nodes count is negative")]
-        fn fails_because_of_invalid_input(args: &str) -> Result<()> {
-            let result = GraphFileGenerator::try_from_args(args);
-
-            // probably should check error type
-            assert!(result.is_err());
-            Ok(())
-        }
+        assert_eq!(actual_error.to_string(), expected_error.to_string());
+        Ok(())
     }
 
-    mod passing_tests {
-        use super::*;
+    #[test_case("--graph-file aaa.txt --nodes-count 5a --edges-count 3 --max-weight 100"; "nodes count is not integer")]
+    #[test_case("--graph-file aaa.txt --nodes-count 5 --edges-count 3a --max-weight 100"; "edges count is not integer")]
+    #[test_case("--graph-file aaa.txt --nodes-count 5 --edges-count 3 --max-weight 100a"; "max weight is not integer")]
+    #[test_case("--graph-file aaa.txt --nodes-count 5 --edges-count 3a"; "missing max weight")]
+    #[test_case("--graph-file aaa.txt --nodes-count 0 --edges-count 3 --max-weight 100"; "nodes count is zero")]
+    #[test_case("--graph-file aaa.txt --nodes-count -5 --edges-count 3 --max-weight 100"; "nodes count is negative")]
+    fn fails_because_of_invalid_input(args: &str) -> Result<()> {
+        let result = GraphFileGenerator::try_from_args(args);
 
-        #[test]
-        fn ok() -> Result<()> {
-            let parameters = GraphFileGenerator::try_from_args(
-                "--graph-file aaa.txt --nodes-count 5 --edges-count 4 --max-weight 100",
-            )
-            .unwrap();
+        // probably should check error type
+        assert!(result.is_err());
+        Ok(())
+    }
+}
 
-            let result = generate_graph(&parameters);
+mod passing_tests {
+    use super::*;
 
-            assert!(result.is_ok());
+    #[test]
+    fn ok() -> Result<()> {
+        let parameters =
+            GraphFileGenerator::try_from_args("--graph-file aaa.txt --nodes-count 5 --edges-count 4 --max-weight 100")
+                .unwrap();
 
-            Ok(())
-        }
+        let result = generate_graph(&parameters);
+
+        assert!(result.is_ok());
+
+        Ok(())
     }
 }
