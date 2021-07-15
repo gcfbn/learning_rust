a834d68b2f61cc8946ceb0cb3316b5772a51720c
gcfbn <bmekarski@interia.pl>
2021-07-15 23:07:43 +0200

    Add doc comments for errors.rs

--- graphs/runner/src/errors.rs
+++ graphs/runner/src/errors.rs
@@ -1,20 +1,32 @@
+#![warn(missing_docs)]
 use graph::BuildGraphError;
 use parse_display::Display;
 use std::io::Error as ioError;
 use thiserror::Error;
 
+/// Result returned by graph generator
 pub type Result<T, E = GenerateGraphError> = std::result::Result<T, E>;
 
+/// Enum containing variants of errors that could occur in bin/main.rs  
 #[derive(Error, Debug)]
 pub enum RunnerError {
+    /// Error connected with building graph from file or string
     #[error("graph builder error - {0}")]
     BuildGraphError(BuildGraphError),
 
+    /// Error connected with generating graph file
     #[error("graph file generator error - {0}")]
     GraphFileGeneratorError(GenerateGraphError),
 
+    /// Error connected with creating subcommand with given name and args
     #[error("failed creating command with command_name='{command_name}' and args={args}")]
-    SubcommandCreatingError { command_name: String, args: String },
+    SubcommandCreatingError {
+        /// cli subcommand name
+        command_name: String,
+
+        /// cli subcommand arguments
+        args: String,
+    },
 }
 
 impl From<BuildGraphError> for RunnerError {
@@ -31,17 +43,28 @@ impl From<GenerateGraphError> for RunnerError {
 
 // -----------------------------------------------------------------------------
 
+/// Enum containing variants of errors that might occur during generating graph
 #[derive(Debug, Display)]
 pub enum GenerateGraphError {
+    /// Connected graph must contain at least n-1 edges (where `n` is number of nodes in the graph)
     #[display("{edges_count} edges is not enough to generate connected graph with {nodes_count} nodes")]
-    TooFewEdgesForConnectedGraph { edges_count: u32, nodes_count: u32 },
+    TooFewEdgesForConnectedGraph {
+        /// number of edges in the graph
+        edges_count: u32,
 
+        /// number of nodes in the graph
+        nodes_count: u32,
+    },
+
+    /// Couldn't create directory specified in output filepath
     #[display("creating directory for output file failed with error - {0}")]
     CreatingDirectoryError(ioError),
 
+    /// Couldn't create file containing graph data
     #[display("creating output file failed with error - {0}")]
     CreatingFileError(ioError),
 
+    /// Couldn't write to output file
     #[display("writing error - {0}")]
     WritingError(ioError),
 }
