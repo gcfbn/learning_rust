6d60a25138e4dc71979842442818c5b385446a72
pdodev <avatar4pl@gmail.com>
2021-07-17 14:04:03 +0200

    Use atty

--- graphs/runner/src/bin/main.rs
+++ graphs/runner/src/bin/main.rs
@@ -1,16 +1,32 @@
 use clap::Clap;
+use std::io::{self, Write};
 use std::process;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 use runner_lib::*;
 
 type Result<T, E = RunnerError> = std::result::Result<T, E>;
+
+fn write_error_message(msg: &str) -> io::Result<()> {
+    let choice = if atty::is(atty::Stream::Stderr) {
+        ColorChoice::Auto
+    } else {
+        ColorChoice::Never
+    };
+
+    let mut out = StandardStream::stderr(choice);
+    out.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
+    write!(&mut out, "error: ")?;
+    out.set_color(ColorSpec::new().set_fg(Some(Color::White)))?;
+    writeln!(&mut out, "{}", msg)
+}
 
 /// Main function that is called when the app starts
 ///
 /// Calls run() function and kills process if it returns an error
 fn main() {
     if let Err(err) = run() {
-        println!("Error: {}", err);
+        write_error_message(&err.to_string()).unwrap();
         process::exit(1);
     }
 }
