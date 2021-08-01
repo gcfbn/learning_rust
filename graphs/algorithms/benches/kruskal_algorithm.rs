c29beca9f5945a7aec939c06d30f39ada3fbce3b
gcfbn <bmekarski@interia.pl>
2021-08-01 19:06:32 +0200

    Remove calling make from algorithms/benches/kruskal_algorithm.rs

--- graphs/algorithms/benches/kruskal_algorithm.rs
+++ graphs/algorithms/benches/kruskal_algorithm.rs
@@ -3,48 +3,27 @@
 extern crate test;
 
 use algorithms::calculate_min_total_weight;
-use graph::{build_graph, Graph};
+use graph::build_graph;
 use std::path::PathBuf;
-use std::process::Command;
 use test::Bencher;
 
-// before each bench
-fn generate_file_and_build_graph(nodes_count: i32) -> Graph {
-    let make_command = format!("make generate_graph_{}_nodes", nodes_count);
-
-    let command = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .args(&["/C", &make_command])
-            .current_dir("./../")
-            .output()
-    } else {
-        Command::new("sh")
-            .args(&["-c", &make_command])
-            .current_dir("./../")
-            .output()
-    };
-
-    assert!(command.is_ok());
-
-    let graph_path = PathBuf::from(format!("benches/data/{}_nodes.txt", nodes_count));
-    build_graph(&graph_path).unwrap()
-}
+// Make sure to run `make benchmarks_data` before running benches
 
 #[bench]
 fn bench_1000_nodes(b: &mut Bencher) {
-    let graph = generate_file_and_build_graph(1000);
+    let graph = build_graph(&PathBuf::from("benches/data/1000_nodes.txt")).unwrap();
     b.iter(|| calculate_min_total_weight(graph.clone()));
 }
 
 #[bench]
 fn bench_10000_nodes(b: &mut Bencher) {
-    let graph = generate_file_and_build_graph(10000);
+    let graph = build_graph(&PathBuf::from("benches/data/10000_nodes.txt")).unwrap();
     b.iter(|| calculate_min_total_weight(graph.clone()));
 }
 
 // causes stack overflow
 #[bench]
 fn bench_100000_nodes(b: &mut Bencher) {
-    let graph = generate_file_and_build_graph(100000);
+    let graph = build_graph(&PathBuf::from("benches/data/100000_nodes.txt")).unwrap();
     b.iter(|| calculate_min_total_weight(graph.clone()));
 }
