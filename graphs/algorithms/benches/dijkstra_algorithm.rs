b16ae3a9e3f1a8f07a0a2146b0fc77ca64c181e5
gcfbn <bmekarski@interia.pl>
2021-08-06 21:49:48 +0200

    Add benchmarks for Dijkstra's algorithm

---
+++ graphs/algorithms/benches/dijkstra_algorithm.rs
@@ -0,0 +1,35 @@
+#![feature(test, command_access)]
+
+extern crate test;
+
+use algorithms::find_shortest_path_length;
+use graph::build_graph;
+use std::path::PathBuf;
+use test::Bencher;
+use utils::PositiveInteger;
+
+// Make sure to run `make benchmarks_data` before running benches
+
+#[bench]
+fn bench_1000_nodes(b: &mut Bencher) {
+    let graph = build_graph(&PathBuf::from("benches/data/1000_nodes.txt")).unwrap();
+    b.iter(|| find_shortest_path_length(&graph.clone(), PositiveInteger::new(1), PositiveInteger::new(1000)));
+}
+
+#[bench]
+fn bench_10000_nodes(b: &mut Bencher) {
+    let graph = build_graph(&PathBuf::from("benches/data/10000_nodes.txt")).unwrap();
+    b.iter(|| find_shortest_path_length(&graph.clone(), PositiveInteger::new(1), PositiveInteger::new(10000)));
+}
+
+#[bench]
+fn bench_100000_nodes(b: &mut Bencher) {
+    let graph = build_graph(&PathBuf::from("benches/data/100000_nodes.txt")).unwrap();
+    b.iter(|| find_shortest_path_length(&graph.clone(), PositiveInteger::new(1), PositiveInteger::new(100000)));
+}
+
+#[bench]
+fn bench_200000_nodes(b: &mut Bencher) {
+    let graph = build_graph(&PathBuf::from("benches/data/200000_nodes.txt")).unwrap();
+    b.iter(|| find_shortest_path_length(&graph.clone(), PositiveInteger::new(1), PositiveInteger::new(200000)));
+}
