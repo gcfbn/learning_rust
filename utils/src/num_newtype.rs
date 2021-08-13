8026366949a5b8d621aa4f2796b8124352af3e63
gcfbn <bmekarski@interia.pl>
2021-08-13 21:25:50 +0200

    Add `Copy` trait to bounds Add tests for `From`

--- utils/src/num_newtype.rs
+++ utils/src/num_newtype.rs
@@ -1,10 +1,42 @@
 use num_traits::Num;
+use std::ops::Deref;
 
-pub struct MyNum<T: Num>(T);
+pub struct MyNum<T: Num + Copy>(T);
 
-impl <T: Num> From<T> for MyNum<T> {
+impl <T: Num + Copy> From<T> for MyNum<T> {
     fn from(t: T) -> Self {
         Self(t)
+    }
+}
+
+impl <T: Num + Copy> Deref for MyNum<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod from {
+        use super::*;
+        use test_case::test_case;
+
+        #[test_case(3i32)]
+        #[test_case(3i8)]
+        #[test_case(3i128)]
+        #[test_case(3u32)]
+        #[test_case(3u8)]
+        #[test_case(3.0f32)]
+        fn from_i32<T: Num + Copy>(val: T) {
+            let my_num = MyNum::from(val);
+
+            // works only with dereference `*` operator unless you implement PartialEq trait
+            assert!(val == *my_num);
+        }
     }
 }
 
