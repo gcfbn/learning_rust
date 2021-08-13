b959cbd9e211b161200c749b09967f59ffcd7096
gcfbn <bmekarski@interia.pl>
2021-08-13 22:10:53 +0200

    Add `derive`, more `PartialEq` and arithmetics - addition

--- utils/src/num_newtype.rs
+++ utils/src/num_newtype.rs
@@ -1,6 +1,7 @@
 use num_traits::Num;
-use std::ops::Deref;
+use std::ops::{Deref, Add};
 
+#[derive(Clone, Copy, Debug)]
 pub struct MyNum<T: Num + Copy>(T);
 
 impl<T: Num + Copy> From<T> for MyNum<T> {
@@ -23,9 +24,39 @@ impl<T: Num + Copy> Deref for MyNum<T> {
     }
 }
 
+impl <T: Num + Copy> PartialEq<MyNum<T>> for MyNum<T> {
+    fn eq(&self, other: &MyNum<T>) -> bool {
+        self.0 == other.0
+    }
+}
+
 impl<T: Num + Copy> PartialEq<T> for MyNum<T> {
     fn eq(&self, other: &T) -> bool {
         self.0.eq(other)
+    }
+}
+
+impl <T: Num + Copy> Add<MyNum<T>> for MyNum<T> {
+    type Output = MyNum<T>;
+
+    fn add(self, rhs: MyNum<T>) -> Self::Output {
+        MyNum::from(self.0 + *rhs)
+    }
+}
+
+impl <T: Num + Copy> Add<&MyNum<T>> for MyNum<T> {
+    type Output = MyNum<T>;
+
+    fn add(self, rhs: &MyNum<T>) -> Self::Output {
+        MyNum::from(self.0 + **rhs)
+    }
+}
+
+impl <T: Num + Copy> Add<T> for MyNum<T> {
+    type Output = MyNum<T>;
+
+    fn add(self, rhs: T) -> Self::Output {
+        MyNum::from(self.0 + rhs)
     }
 }
 
@@ -58,7 +89,24 @@ mod tests {
             let i32_vec: Vec<i32> = vec![1, 2, 3];
 
             let my_num_vec: Vec<MyNum<i32>> = i32_vec.iter().map(From::from).collect();
-            assert!(my_num_vec == i32_vec)
+            assert_eq!(my_num_vec, i32_vec)
+        }
+    }
+
+    mod arithmetics {
+        use super::*;
+
+        mod add {
+            use super::*;
+
+            #[test]
+            fn add_my_num_and_my_num() {
+                let one = MyNum::from(1);
+                let two = MyNum::from(2);
+                let three = MyNum::from(3);
+
+                assert_eq!(one + two, three);
+            }
         }
     }
 }
