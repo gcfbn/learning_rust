7e63bd964ace3d0f1241cba3600398edb708e407
gcfbn <bmekarski@interia.pl>
2021-09-03 22:35:34 +0200

    Add comments and info about running the example

--- utils/examples/use_tracing_crate/main.rs
+++ utils/examples/use_tracing_crate/main.rs
@@ -1,12 +1,19 @@
-/// You can run this program with the following command e.g.:
+/// Please install Jaeger to see results of this example:
+/// https://www.jaegertracing.io/docs/1.25/getting-started/
 ///
-/// RUST_LOG=error,use_tracing_crate=warn cargo run --example use_tracing_crate --features tracing_example
+/// Run Jaeger and navigate to http://localhost:16686 to access the Jaeger UI.
 ///
+/// Then, run the program with the following command e.g.:
+/// RUST_LOG=trace&&cargo run --example use_tracing_crate --features tracing_example
+///
+/// Your Jaeger UI should look like similar to this:
+/// https://imgur.com/a/5fZuLND
+
 mod cmd_args;
 
 use std::fmt::Debug;
 use thiserror::Error;
-use tracing::{warn, error, trace, Level, span, };
+use tracing::{warn, error, trace, Level, span};
 use tracing_subscriber::{fmt::Subscriber, EnvFilter, prelude::*, util::SubscriberInitExt, registry::Registry};
 use utils::ApplicationRunner;
 use opentelemetry::{global, sdk::trace::Tracer};
@@ -47,13 +54,19 @@ impl ApplicationRunner for App {
     }
 
     fn configure_logging(&self) {
+        // send opentelemetry data to Jaeger
         global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
 
+        // write logging messages to .log file
         let file_subscriber = Subscriber::new()
+            // allow non-ANSI characters
             .with_ansi(false)
+            // create new file every minute
             .with_writer(|| tracing_appender::rolling::minutely("./.logs", "use_logger_with_state"))
+            // use own time formatter
             .with_timer(MySystemTimeFormatter);
 
+        // write logging messages to standard output
         let stdout_subscriber = Subscriber::new()
             .with_writer(std::io::stdout)
             .with_timer(MySystemTimeFormatter);
@@ -68,6 +81,7 @@ impl ApplicationRunner for App {
             .with(telemetry)
             .init();
 
+        // create and enter test_span, which should be visible in Jaeger UI
         let test_span = span!(Level::TRACE, "test_span");
         let _enter = test_span.enter();
         trace!("entered test_span");
